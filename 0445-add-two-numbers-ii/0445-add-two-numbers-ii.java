dbc189e8f27a71982453def41280c6ac2ee803df
Sujitha M <sujithamurugan17@gmail.com>
2026-03-23 12:10:00 +0530

    Time: 3 ms (44.5%), Space: 46.9 MB (13.05%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        // Push values into stacks
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        ListNode head = null;
+
+        // Process stacks
+        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+            int sum = carry;
+
+            if (!s1.isEmpty()) sum += s1.pop();
+            if (!s2.isEmpty()) sum += s2.pop();
+
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;   // insert at front
+            head = node;
+
+            carry = sum / 10;
+        }
+
+        return head;
+    }
+}
